fe78c471c4433ffdfa139de87946e915b5703a83
Samijagirdar <mdsamijagirdar@gmail.com>
2026-05-02 12:02:52 +0530

    feat: implement role-based login handling in App.jsx with conditional rendering for admin and employee dashboards

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Login from "./components/Auth/Login";
 import EmployeeDashboard from "./components/Dashboard/EmployeeDashboard";
 import AdminDashboard from "./components/Dashboard/AdminDashboard";
+import { getLocalStorage, setLocalStorage } from "./utils/localStorage";
 
 const App = () => {
+  const [user, setUser] = useState(null);
+
+  const handleLogin = (email, password) => {
+    if (email == "admin@me.com" && password == "123") {
+      setUser("admin");
+    } else if (email == "user@me.com" && password == "123") {
+      setUser("employee");
+    } else {
+      alert("Invalid Credentials");
+    }
+  };
+
   return (
     <div className="p-10 h-screen w-full">
       <>
-        {/* <Login /> */}
-        {/* <EmployeeDashboard /> */}
-        <AdminDashboard />
+        {!user && <Login handleLogin={handleLogin} />}
+
+        {user === "admin" && <AdminDashboard />}
+        {user === "employee" && <EmployeeDashboard />}
       </>
     </div>
   );
